4cc66248423e8d7963da5410169bccc1835e6685
Zain Irfan <z_irfan7862@hotmail.com>
2024-12-31 22:29:12 +0000

    Add files via upload
    Pet Appointment Generator program in Python

---
+++ Pet_Appointment_Generator.py
@@ -0,0 +1,12 @@
+print ("welcome to the pet generator program!")
+health=input ("is my pet a healthy weight?")
+print (health)
+diet=input ("what should I feed my pet?")
+print (diet)
+vacination=input ("what vacination does my pet need?")
+print (vacination)
+exercise=input ("is my pet getting enough exercise?")
+print (exercise)
+print ("your pets health could be determined by:" + diet + "" + vacination)
+
+
